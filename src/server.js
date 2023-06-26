3145eb3fb01045b34c9ddcad7fa926fbfecf32a2
Enfis123 <toncenose123@gmail.com>
2023-06-25 21:41:30 -0500

    All

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Configura la ruta para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Inicia el servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor web iniciado en el puerto ${port}`);
+});
