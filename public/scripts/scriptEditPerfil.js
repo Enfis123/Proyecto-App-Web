e56c0f081c0d2bca01aa80677df43a15a08549ff
Tureright <sonee@outlook.es>
2023-09-03 08:08:33 -0500

    Cambios para editar los datos del perfil del usuario logeado.

--- public/scripts/scriptEditPerfil.js
+++ public/scripts/scriptEditPerfil.js
@@ -1,22 +1,39 @@
-// scriptEditPerfil.js
-document.addEventListener('DOMContentLoaded', function () {
-    const profileForm = document.getElementById('profile-form');
-  
-    // Event listener para el envío del formulario
-    profileForm.addEventListener('submit', function (e) {
-      e.preventDefault(); // Evita que se envíe el formulario automáticamente
-  
-      // Obtener los valores ingresados por el usuario
-      const firstName = document.getElementById('first-name').value;
-      const lastName = document.getElementById('last-name').value;
-      const email = document.getElementById('email').value;
-  
-      // Realizar aquí el código para actualizar el perfil en el servidor (por ejemplo, mediante una solicitud AJAX)
-  
-      // Ejemplo de una solicitud AJAX ficticia
-      setTimeout(function () {
-        alert('Perfil actualizado correctamente');
-      }, 1000);
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener referencias a los elementos del formulario de edición de perfil
+    var firstNameInput = document.getElementById("first-name");
+    var lastNameInput = document.getElementById("last-name");
+    var emailInput = document.getElementById("email");
+    var guardarCambiosBtn = document.getElementById("btnGuardarCambiosPerfil");
+    var mensajeError = document.getElementById("mensajeError");
+
+    // Agregar un evento al botón "Guardar Cambios"
+    guardarCambiosBtn.addEventListener("click", function (event) {
+        event.preventDefault(); // Evitar el envío del formulario por defecto
+
+        // Obtener los valores ingresados por el usuario
+        var firstName = firstNameInput.value;
+        var lastName = lastNameInput.value;
+
+        // Enviar los datos al servidor para guardar los cambios de perfil
+        fetch('/api/editarPerfil', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ firstName, lastName, email: emailInput.value })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    // Cambios guardados con éxito
+                    window.location.href = "/perfil";
+                } else {
+                    // Mostrar mensaje de error del servidor si es necesario
+                    mensajeError.textContent = "Error al guardar los cambios.";
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     });
-  });
-  
+});
---
+++ src/api/editarPerfilApi.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const conn = require('./databaseConfig');
+
+// Función para obtener el valor de una cookie por su nombre
+function getCookieValue(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+    return null; // Si no se encuentra la cookie
+}
+
+
+
+// Ruta para obtener un perfil de usuario por su ID
+router.get('/', (req, res) => {
+    const { usuarioId } = req.cookies; // Obtener el ID del usuario de la cookie
+
+    const query = 'SELECT * FROM usuarios WHERE id = $1';
+    conn.query(query, [usuarioId])
+        .then(result => {
+            if (result.rows.length > 0) {
+                // Se encontró un perfil con el ID del usuario
+                res.json(result.rows[0]);
+            } else {
+                // No se encontró un perfil con el ID proporcionado
+                res.status(404).json({ error: 'Perfil no encontrado' });
+            }
+        })
+        .catch(err => {
+            console.error('Error al obtener el perfil:', err);
+            res.status(500).json({ error: 'Error al obtener el perfil' });
+        });
+});
+
+// Ruta para actualizar un perfil de usuario
+router.put('/', (req, res) => {
+
+    const { usuarioId } = req.cookies; // Obtener el ID del usuario de la cookie
+    const { firstName, lastName, email } = req.body;
+
+    const query = 'UPDATE usuarios SET nombre = $1, apellido = $2, email = $3 WHERE id = $4';
+    conn.query(query, [firstName, lastName, email, usuarioId])
+        .then(() => {
+            res.json({ success: true });
+        })
+        .catch(err => {
+            console.error('Error al actualizar el perfil:', err);
+            res.status(500).json({ error: 'Error al actualizar el perfil' });
+        });
+});
+
+module.exports = router;
